565bde701f068aae239d8e2a8729ec895af0e8a5
Thiago Lopes <thiago.lopes@pyxesportal.com>
2022-02-08 08:10:19 -0500

    Creating the project and the firsts components

---
+++ angular-base/src/app/views/listado/entrada/entrada.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Entrada } from 'src/app/shared/interfaces/interfaces';
+
+@Component({
+  selector: 'app-entrada',
+  templateUrl: './entrada.component.html',
+  styleUrls: ['./entrada.component.css']
+})
+export class EntradaComponent implements OnInit {
+
+  @Input()
+  public entrada: Entrada;
+
+  constructor() {
+    this.entrada = {
+      titulo: '',
+      resumen: ''
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-base/src/app/views/listado/listado.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Entrada } from 'src/app/shared/interfaces/interfaces';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent implements OnInit {
+
+  public listadoEntradas: Entrada[];
+
+  constructor() {
+    this.listadoEntradas = [
+      {
+        titulo: 'Introducción a Angular',
+        resumen: 'En esta lección realizaremos una pequeña introducción al mundo de desarrollo con Angular'
+      },
+      {
+        titulo: 'Typescript como lenguaje para Angular',
+        resumen: 'Breve recorrido por el lenguaje de Typescript como base para desarrollar en Angular'
+      },
+      {
+        titulo: 'Componentes en Angular',
+        resumen: 'Aprenderemos a usar los componentes en Angular y el porqué de su importancia'
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+  }
+
+  public mostrarTitulo(titulo: string): void {
+    alert(`Entrada seleccionada: ${ titulo }.`);
+  }
+
+}
